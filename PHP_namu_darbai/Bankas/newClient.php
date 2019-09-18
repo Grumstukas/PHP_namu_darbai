089afd3f84ff30b5226c5a023ec565d5e55ea6e5
Grumstukas <tartenaite.toma@gmail.com>
2019-09-18 14:24:46 +0300

    Bank baigtas

--- PHP_namu_darbai/Bankas/newClient.php
+++ PHP_namu_darbai/Bankas/newClient.php
@@ -4,23 +4,24 @@ session_start();
 
 include __DIR__ . '/hello_my_db.php';
 
-if (!empty($_POST)) {
+if (!empty($_POST))
+{
 
     $sql = "SELECT id FROM b_clients WHERE name=? AND surname=?;";
     $stmt = $pdo->prepare($sql);
     $stmt->execute([$_POST['name'], $_POST['surname']]); //execute grazina tru arba false pagal rezultata
     $id = $stmt->fetch();
 
-    if(!isset($id['id']))
+    if (!isset($id['id']))
     {
-    //naujas
-    $stmt = $pdo->prepare ("INSERT INTO b_clients (name, surname) VALUES(?, ?);");
-    $stmt->execute([$_POST['name'], $_POST['surname']]);
-    $sql = "SELECT LAST_INSERT_ID();";
-    $stmt = $pdo->query($sql);
-    $last_id = $stmt->fetch();
-    $client_id = array_shift($last_id);
-    //naujo pabaiga
+        //naujas
+        $stmt = $pdo->prepare("INSERT INTO b_clients (name, surname) VALUES(?, ?);");
+        $stmt->execute([$_POST['name'], $_POST['surname']]);
+        $sql = "SELECT LAST_INSERT_ID();";
+        $stmt = $pdo->query($sql);
+        $last_id = $stmt->fetch();
+        $client_id = array_shift($last_id);
+        //naujo pabaiga
     }
     else
     {
@@ -32,7 +33,7 @@ if (!empty($_POST)) {
     $stmt->execute([0, rand(1000000000, 9999999999), $client_id]);
 
 
-    header('Location: '.$localHostAdress.'index.php');/*po posto reikia puslapį persiųsti */
+    header('Location: ' . $localHostAdress . 'index.php');/*po posto reikia puslapį persiųsti */
     die(); //naršykle daugiau kieko negaus - tegu eina dirbti
 }
 ?>
--- PHP_namu_darbai/Bankas/reduce.php
+++ PHP_namu_darbai/Bankas/reduce.php
@@ -3,26 +3,43 @@ session_start();
 include __DIR__ . '/hello_my_db.php';
 
 echo '<div style="font-size: 20px; font-weight: 800; margin: 20px;">
- Sveiki '.$_SESSION['client_name'].' '.$_SESSION['client_surname'].' jūsų sąskaitoje šiuo metu yra '.$_SESSION['client_balance'].'
+ Sveiki ' . $_SESSION['client_name'] . ' ' . $_SESSION['client_surname'] . ' jūsų sąskaitoje šiuo metu yra ' . $_SESSION['client_balance'] . '
  <br> pasirinkite pinigų sumą kuri bus nuskaityta iš jūsų sąskaitos</div>';
 
-if (!empty($_POST)) {
+if (!empty($_POST))
+{
     $difference = ($_SESSION['client_balance'] - $_POST['amount']);
-    if ($difference >= 0) {
+    if ($difference >= 0)
+    {
 
-    $stmt = $pdo->prepare('UPDATE b_clients SET amount = amount - ? WHERE id = ?');
+        $stmt = $pdo->query("
+        SELECT
+         t1.id as client_id,
+         t1.name,
+         t1.surname,
+         t2.amount,
+         t2.acc_number,
+         t2.id as acountID
+         FROM b_clients t1
+        INNER JOIN  b_accounts t2
+        ON t1.id = t2.client_id
+        ORDER BY t1.id;
+        ");
 
-    $stmt->execute([$_POST['amount'], $_SESSION['client_id']]);
 
-    header('Location: '.$localHostAdress.'index.php');/*po posto reikia puslapį persiųsti */
-    die(); //naršykle daugiau kieko negaus - tegu eina dirbti
+        $stmt = $pdo->prepare('UPDATE b_accounts SET amount = amount - ? WHERE acc_number = ?');
+
+        $stmt->execute([$_POST['amount'], $_SESSION['acc_number']]);
+
+        header('Location: ' . $localHostAdress . 'index.php');/*po posto reikia puslapį persiųsti */
+        die(); //naršykle daugiau kieko negaus - tegu eina dirbti
     }
     else
     {
         echo '<div>Apgailestaujame, sąskaistoje neužtenka pinigų<br>
                 arba bandykite išleisti mažesnę sumą<br>
                 arba grįžkite į banko priimamajį</div>
-<button><a href="'.$localHostAdress.'index.php" style="text-decoration: none; color: black;"> Grįžti atgal</a></button>';
+<button><a href="' . $localHostAdress . 'index.php" style="text-decoration: none; color: black;"> Grįžti atgal</a></button>';
     }
 }
 
